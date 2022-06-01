1439b8c20033630f1b7145b27aa5f83f4158b146
julia-boop <crdr.jc@gmail.com>
2022-05-31 23:34:34 -0300

    funcionalidad del mail

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,36 +1,55 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import './Contact.css'
+import emailjs from 'emailjs-com';
+import Fade from 'react-reveal'
+
 
 function Contact() {
+    const form = useRef();
+    let succesmsg = document.querySelector('.success-msg')
+    let formsent = document.querySelector('form')
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_dcixk5v', 'template_gmj3buc', form.current, 'ta3gMWZHFw3Nx_YzG')
+        .then((result) => {
+            succesmsg.innerHTML = '<p>El mensaje ha sido enviado con exito</p>'
+            formsent.reset()
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
     return (
         <div className="contact-container" id="contact">
             <div className="links-container">
                 <div className="link-item">
-                    <img src="/icons/linkedin-brands.svg" alt="linkedin"></img>
+                    <img src="/icons/linkedin-brands.png" alt="linkedin"></img>
                     <a href="https://www.linkedin.com/company/lynx-bursatil/mycompany/">LinkedIn Lynx Valores SA</a>
                 </div>
                 <div className="link-item">
-                    <img src="/icons/map-pin-solid.svg" alt="map-pin"></img>
+                    <img src="/icons/map-pin-solid.png" alt="map-pin"></img>
                     <a href="https://goo.gl/maps/9DH16Aj5o8vn8fw99">Av. Corrientes 330, CABA</a>
                 </div>
                 <div className="link-item">
-                    <img src="/icons/envelope-solid.svg" alt="envelope"></img>
+                    <img src="/icons/envelope-solid.png" alt="envelope"></img>
                     <a href="mailto:info@lynxbursatil.com.ar">info@lynxbursatil.com.ar</a>
                 </div>
                 <div className="link-item">
-                    <img src="/icons/phone-solid.svg" alt="phone"></img>
+                    <img src="/icons/phone-solid.png" alt="phone"></img>
                     <a>11-4394-5969</a>
                 </div>
             </div>
             <div className="form-container">
                 <h1>Contacto</h1>
-                <form>
-                    <input placeholder="Nombre" type="text"></input>
-                    <input placeholder="Apellido" type="text"></input>
-                    <input placeholder="Email" type="email"></input>
-                    <input placeholder="Telefono" type="number"></input>
-                    <textarea placeholder="Mensaje"></textarea>
-                    <button>Enviar</button>
+                <div className="success-msg"></div>
+                <form ref={form} onSubmit={sendEmail}>
+                    <input placeholder="Nombre y Apellido" type="text" name="user_name"></input>
+                    <input placeholder="Email" type="email" name="user_email"></input>
+                    <input placeholder="Telefono" type="text" name="user_phone"></input>
+                    <textarea placeholder="Mensaje" name="message"></textarea>
+                    <button type="submit" value="Send">Enviar</button>
                 </form>
             </div>
         </div>
